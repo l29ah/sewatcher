a5bc74416710cd80ab03a9059220f202bfe4a0ce
Iaroslav Gridin <voker57@gmail.com>
2020-09-09 17:55:27 +0300

    make save parsing more fault-tolerant

--- sewatcher.rb
+++ sewatcher.rb
@@ -3,6 +3,7 @@
 
 require 'rb-inotify'
 require 'pathname'
+require 'open3'
 
 def get_savename(filename)
 	pt = Pathname.new(filename)
@@ -10,17 +11,20 @@ def get_savename(filename)
 end
 
 def get_turn(filename)
-	sf = IO.popen(["unzip", "-c", "-P", "GarfieldJonesCat", filename])
+	out, err, status = Open3.capture3("unzip", "-c", "-P", "GarfieldJonesCat", filename)
 
-	d = sf.read(5000)
+	if !status.success?
+		# it's not a save yet, likely
+		return nil
+	end
 
 	desc_string = "This is the base file for creation the random planets."
 
-	desc_pos = (d.index(desc_string))
+	desc_pos = (out.index(desc_string))
 
 	turn_pos = desc_pos + desc_string.size+1
 
-	d[turn_pos].ord
+	out[turn_pos].ord
 end
 
 def your_number(savefile)
@@ -43,6 +47,7 @@ initial_message = "sewatcher initialized\n"
 
 Dir.glob("#{ARGV[0]}/*.se1") do |savefile|
 	turn = get_turn(savefile)
+	next unless turn
 	savename = get_savename(savefile)
 	itsyou = if your_number(savename) == turn then " (you)" else "" end
 	$saves[savename] = turn
@@ -51,10 +56,11 @@ end
 
 system("notify-send", initial_message)
 
-notifier.watch(ARGV[0], :moved_to) do |event|
+notifier.watch(ARGV[0], :moved_to, :close_write) do |event|
 	next unless event.name =~ /.se1$/
 	fname = "#{ARGV[0]}/#{event.name}"
 	turn = get_turn(fname)
+	next unless turn
 	you = your_number(event.name)
 	if $saves[event.name] != turn && (!you || turn == you)
 		itsyou = if you == turn then " (you)" else "" end
