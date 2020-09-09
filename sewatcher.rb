f2e2ebe953b01040c7b3061f0f8df48ee54c1c29
Iaroslav Gridin <voker57@gmail.com>
2020-09-09 18:07:14 +0300

    always update turn number

--- sewatcher.rb
+++ sewatcher.rb
@@ -62,9 +62,9 @@ notifier.watch(ARGV[0], :moved_to, :close_write) do |event|
 	turn = get_turn(fname)
 	next unless turn
 	you = your_number(event.name)
+	$saves[event.name] = turn
 	if $saves[event.name] != turn && (!you || turn == you)
 		itsyou = if you == turn then " (you)" else "" end
-		$saves[event.name] = turn
 		system("notify-send", "sewatcher: [#{event.name}] player #{turn}#{itsyou}")
 	end
 end
