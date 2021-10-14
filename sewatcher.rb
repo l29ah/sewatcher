cfe80b3eba715851797a5d85c9b5070338c6db3a
Iaroslav Gridin <voker57@gmail.com>
2021-10-14 21:41:00 +0300

    Use config, watch only specified games

--- sewatcher.rb
+++ sewatcher.rb
@@ -5,6 +5,7 @@ require 'rb-inotify'
 require 'pathname'
 require 'open3'
 require 'optparse'
+require 'yaml'
 
 def get_savename(filename)
 	pt = Pathname.new(filename)
@@ -46,10 +47,6 @@ $options = {}
 OptionParser.new do |opts|
 	opts.banner = "Notifies you of new turns in Shadow Empire games in given dirs.
 	Usage: #{$0} [$options] <savedir> [<savename:playerno> ...]"
-
-	opts.on("-p", "--parse", "Parse all save files once, notify and exit") do |v|
-		$options[:parse] = true
-	end
 	
 	opts.on("-n", "--no-notify", "Don't notify-send") do |v|
 		$options[:no_notify] = true
@@ -58,20 +55,10 @@ OptionParser.new do |opts|
 	opts.on("-s", "--stdout", "Notify on stdout") do |v|
 		$options[:stdout] = true
 	end
-	
-	opts.on("-a", "--all", "Display all turns, not only yours") do |v|
-		$options[:all] = true
-	end
-	
-	opts.on("-w", "--watch", "Like --all, but display others' turns only on stdout. Implies --all, --stdout") do |v|
-		$options[:all] = true
-		$options[:stdout] = true
-		$options[:stdout_others] = true
-	end
 end.parse!
 
+config = YAML.load_file(ARGV[0])
 notifier = INotify::Notifier.new
-
 
 $saves = Hash.new
 $mynumbers = Hash.new
@@ -84,35 +71,44 @@ end
 
 initial_message = "sewatcher initialized\n"
 
-Dir.glob("#{ARGV[0]}/*.se1") do |savefile|
-	turn = get_turn(savefile)
+config["games"].each do |game|
+	filename = Pathname::new(game["file"]).expand_path.to_s
+	turn = get_turn(filename)
 	next unless turn
-	savename = get_savename(savefile)
-	itsyou = if your_number(savename) == turn then " (you)" else "" end
-	$saves[savename] = turn
-	initial_message << "[#{savename}] player #{turn}#{itsyou}\n"
+	itsyou = if game["turn"] == turn then " (you)" else "" end
+	$saves[filename] = turn
+	initial_message << "[#{game["name"] || filename}] player #{turn}#{itsyou}\n"
 end
 
 do_notification initial_message
 
 exit if $options[:parse]
 
-notifier.watch(ARGV[0], :moved_to, :close_write) do |event|
-	next unless event.name =~ /.se1$/
-	fname = "#{ARGV[0]}/#{event.name}"
-	turn = get_turn(fname)
-	next unless turn
-	you = your_number(event.name)
-	if $saves[event.name] != turn && ($options[:all] || you == turn || you == :all)
-		itsyou = if you == turn then " (you)" else "" end
-		notification = "[#{event.name}] player #{turn}#{itsyou}"
-		if $options[:stdout_others] && you != turn
-			puts notification
-		else
-			do_notification notification
+threads = []
+
+config["games"].each do |game|
+	Thread.new do
+		fullname = Pathname.new(game["file"]).expand_path
+		dir = fullname.dirname.to_s
+		filename = fullname.to_s
+		notifier.watch(dir, :moved_to, :close_write) do |event|
+			event_filename = Pathname.new("#{ARGV[0]}/#{event.name}").expand_path.to_s
+			next unless event_filename == filename
+			turn = get_turn(filename)
+			next unless turn
+			itsyou = if game["turn"] == turn then " (you)" else "" end
+			notification = "[#{game["name"] || filename}] player #{turn}#{itsyou}"
+			if $saves[filename] != turn && game["turn"] == turn
+				if $options[:stdout]
+					puts notification
+				end
+				if !$options[:no_notify]
+					do_notification notification
+				end
+			end
+			$saves[filename] = turn
 		end
 	end
-	$saves[event.name] = turn
 end
 
 notifier.run
